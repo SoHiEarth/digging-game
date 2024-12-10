130369a215c8dbbe3af91a701ca705c34170b348
SoHiEarth <126925211+SoHiEarth@users.noreply.github.com>
2024-12-10 20:58:29 +0900

    Add color animation

--- include/animate.hpp
+++ include/animate.hpp
@@ -15,7 +15,101 @@ struct AnimationFrame {
   int yOffset = 0;
   int wOffset = 0;
   int hOffset = 0;
+  int rOffset = 0;
+  int gOffset = 0;
+  int bOffset = 0;
+  int aOffset = 0;
   int timeUntilNextFrame = 1;
+};
+
+class Animator_Color {
+  std::vector<AnimationFrame> frames{};
+  bool isPlaying = false;
+
+  void PlayAnimation() {
+    isPlaying = true;
+    for (AnimationFrame& frame : frames) {
+      colorToAnimate.r += frame.rOffset;
+      colorToAnimate.g += frame.gOffset;
+      colorToAnimate.b += frame.bOffset;
+      colorToAnimate.a += frame.aOffset;
+      SDL_Delay(frame.timeUntilNextFrame);
+    }
+    isPlaying = false;
+  }
+
+  public:
+  std::string AnimationSourceFile;
+  SDL_Color& colorToAnimate;
+  std::thread animationThread;
+
+  Animator_Color(SDL_Color& color) : colorToAnimate(color) {}
+
+  void Play() {
+    if (animationThread.joinable()) {
+      animationThread.join();
+    }
+    if (!isPlaying) animationThread = std::thread(&Animator_Color::PlayAnimation, this);
+  }
+
+  void LoadAnimation(const std::string& sourceFile) {
+    AnimationSourceFile = sourceFile;
+    // Load animation from file
+    // Animations are loaded as the following format
+    // {specify properties (rOffset, etc.)}:{specify value of offset} {}:{} timeUntilNextFrame
+    // Example:
+    // {rOffset}:{10} {gOffset}:{0} {bOffset}:{0} {aOffset}:{0} 100
+
+    std::ifstream file(sourceFile);
+    if (!file.is_open()) {
+      throw std::runtime_error("Could not open file: " + sourceFile);
+    }
+
+    std::string line;
+    while (std::getline(file, line)) {
+      AnimationFrame frame;
+      std::istringstream iss(line);
+      std::string token;
+      while (iss >> token) {
+        // Read into int first, then cast to Uint8 to avoid issues
+        int r, g, b, a;
+        if (token == "{rOffset}:") {
+          iss >> r;
+        }
+        else if (token == "{gOffset}:") {
+          iss >> g;
+        }
+        else if (token == "{bOffset}:") {
+          iss >> b;
+        }
+        else if (token == "{aOffset}:") {
+          iss >> a;
+        }
+        else if (token == "{timeUntilNextFrame}:") {
+          iss >> frame.timeUntilNextFrame;
+        }
+
+        else {
+          std::cout << "Invalid token: " << token << " on file " << sourceFile << "\n";
+          throw std::runtime_error("Invalid token: " + token + " on file " + sourceFile + ", file may be corrupt");
+        }
+
+        frame.rOffset = static_cast<Uint8>(r);
+        frame.gOffset = static_cast<Uint8>(g);
+        frame.bOffset = static_cast<Uint8>(b);
+        frame.aOffset = static_cast<Uint8>(a);
+      }
+      frames.push_back(frame);
+    }
+
+    file.close();
+
+    if (frames.empty()) {
+      throw std::runtime_error("No frames found in file: " + sourceFile);
+    }
+
+    std::cout << "Loaded animation from file: " << sourceFile << "\n";
+  }
 };
 
 class Animator_Rect {
